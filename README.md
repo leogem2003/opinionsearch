c551bb553ab96c8571910b11da5958bb50bca595
Steve <steve@ttlns.ch>
2026-09-09 09:42:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Opinionsearch
+
+## Features
+TODO: screeshots
+
+1. Discover and visualize other opinions
+2. Filter by categories, location and time
+
+## Limitations
+1. Since this is a website, it is centralized and controlled by a single entity
+2. There is currently no bot protection implemented to prevent inauthentic behavior
+3. Language always is ambiguous to some degree
+## License
+See [LICENSE.txt](LICENSE.txt)
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Python development environment with Astral uv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            uv
+          ];
+          shellHook = ''
+            export UV_PYTHON_PREFERENCE="system"
+          '';
+        };
+      });
+    };
+}
